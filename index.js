94effac1bc2ea17bb7e023849d84cea002f41541
juan.carranza <juan.carranza@amitechsolutions.com>
2021-11-21 10:43:24 -0600

    add an example to access the DOM

---
+++ index.js
@@ -0,0 +1,16 @@
+function suma() {
+    return 200;
+}
+
+console.log(suma());
+
+
+let paragraphs=document.getElementsByTagName("p");
+console.log(paragraphs);
+if(paragraphs.length>0){
+    let paragraph=paragraphs[0];
+
+paragraph.innerText="Cambiando a Bienvenidos al Bootcamp!";
+
+}
+
