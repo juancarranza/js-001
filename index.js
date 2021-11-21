abac07314ed9bff72926911ff06e07d31c3cd774
juan_carranza <juan.carranza1804@gmail.com>
2021-11-21 11:03:42 -0600

    changing the let(variable) for const(constant) (using best practices)

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ console.log(suma());
 const paragraphs=document.getElementsByTagName("p");
 console.log(paragraphs);
 if(paragraphs.length>0){
-    //new comment
-    let paragraph=paragraphs[0];
+    //try to use always const instead of the let or var, because this could bring bugs to our code if someone is using it in another function/code.
+    const paragraph=paragraphs[0];
+    paragraph.innerText="Cambiando a Bienvenidos al Bootcamp!";
 
-paragraph.innerText="Cambiando a Bienvenidos al Bootcamp!";
+}
 
+
+if(paragraphs.length>1){
+    //try to use always const instead of the let or var, because this could bring bugs to our code if someone is using it in another function/code.
+    const paragraph=paragraphs[1];
+    const fecha=new Date();
+    paragraph.innerText="Cambiando al otro Bienvenidos. Length: "+paragraphs.length+" ("+fecha+")";
 }
 
