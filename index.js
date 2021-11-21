ea16cf25b4a6b74b220fab5572366ab08d6de5bc
juan_carranza <juan.carranza1804@gmail.com>
2021-11-21 10:47:13 -0600

    adding a comment.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ console.log(suma());
 let paragraphs=document.getElementsByTagName("p");
 console.log(paragraphs);
 if(paragraphs.length>0){
+    //new comment
     let paragraph=paragraphs[0];
 
 paragraph.innerText="Cambiando a Bienvenidos al Bootcamp!";
