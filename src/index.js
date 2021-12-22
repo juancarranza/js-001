1f9450de950fbf2da5370512fea4e9e519f8c681
juan_carranza <juan.carranza1804@gmail.com>
2021-12-22 13:27:25 -0600

    Video: Configurar wepack con React - Parte 2. Completo.

--- src/index.js
+++ src/index.js
@@ -18,12 +18,25 @@
 
 import React from "react";
 import ReactDom from "react-dom";
+import "./index.css";//para que esto funcione seguir los siguientes pasos 
+/**
+ * npm install -D style-loader css-loader 
+ * se agrega esto al webpack.config.json
+ * {
+        use: ["styles-loader", "css-loader"],
+        test: /.css$/
+   }
+ * 
+ * 
+ */
+import logo from "./logo.PNG";
 
 
 const container =document.getElementById("root");
 const App = () => (
-    <div>
-        <h1>Hola mundo desde React</h1>
+    <div className="app">
+        <h1>Hola mundo desde React!</h1>
+        <img src = {logo} />
     </div>
 );
 
