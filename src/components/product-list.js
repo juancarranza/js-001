11f9a6d4c2a48ddf61761c623a73658fc4cff7f6
juan_carranza <juan.carranza1804@gmail.com>
2021-12-24 14:54:26 -0600

    Video: Aplicacion con React. Completo.

--- src/components/product-list.js
+++ src/components/product-list.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const ProductItem = (prop) => {
     const item = prop.item;
+    const acciones = prop.acciones;
     return <tr>
         <td>{item.codigo}</td>
         <td>{item.nombre}</td>
@@ -10,12 +11,22 @@ const ProductItem = (prop) => {
         <td>{item.total}</td>
         <td>
             <div className="btn-group">
-                <a title="Editar" href="#" className="btn btn-sm btn-outline-secondary">
+                <a 
+                    title="Editar" 
+                    href="#" 
+                    className="btn btn-sm btn-outline-secondary" 
+                    onClick={() => acciones.seleccionar(item.codigo)}
+                    >
                     <i className="bi bi-pencil-square"></i>
                 </a> 
             
-                <a title="Eliminar" href="#" className="btn btn-sm btn-outline-danger">
-                <i className="bi bi-trash"></i>
+                <a 
+                    title="Eliminar" 
+                    href="#" 
+                    className="btn btn-sm btn-outline-danger"
+                    onClick={() => acciones.eliminar(item.codigo)}
+                    >
+                    <i className="bi bi-trash"></i>
                 </a> 
             </div>
         </td>
@@ -33,6 +44,24 @@ const ProductList = () => {
         
         }
     ];
+
+    const seleccionar = (codigo) => {
+        console.log('seleccionar: ', codigo);
+    };
+
+    const eliminar = (codigo) => {
+        console.log('eliminar: ', codigo);
+    };
+
+    const acciones = {
+        seleccionar,
+        eliminar
+    };
+
+    const cantidadTotal=sum(productos, x=> x.cantidad);
+    const precioTotal=sum(productos, x=> x.precio);
+    const granTotal=sum(productos, x=> x.total);
+ 
     return <table className="table">
         <thead>
             <tr>
@@ -44,17 +73,24 @@ const ProductList = () => {
             </tr>
         </thead>
         <tbody>
-            {productos.map(item => <ProductItem key={item.codigo} item = {item}  />)} 
+            {productos.map(item => <ProductItem key={item.codigo} item = {item} acciones={acciones} />)} 
         </tbody>
         <tfoot>
             <tr>
                 <td colSpan="2">Totales:</td>
-                <td id="cantidad-total">0</td>
-                <td id="precio-total">0</td>
-                <td id="gran-total">0</td>
+                <td>{cantidadTotal}</td>
+                <td>{precioTotal}</td>
+                <td >{granTotal}</td>
             </tr>
         </tfoot>
     </table>
 };
 
+function sum(elementos,selector){
+    return elementos
+                .map(selector)
+                .reduce((a,b) => a + b,0);
+}
+
+//export { ProductItem, ProductList};
 export default ProductList;
