d9b1bd3e2ca5560a6592439c3cef3151ab929893
juan_carranza <juan.carranza1804@gmail.com>
2022-01-12 01:28:37 -0600

    Backend con Express - Parte 1. Video Completo.

--- servidor/index.js
+++ servidor/index.js
@@ -2,20 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
+
+let lastId=0;
 const productos = [
-    {
-        codigo:1,
-        nombre: "producto 1",
-        precio: 100,
-        cantidad: 10
-    },
-    {
-        codigo:2,
-        nombre: "producto 2",
-        precio: 50,
-        cantidad: 10
-    }, 
+   
 ];
+
 
 app.use(bodyParser.json({type: 'application/json'}));
 app.use(logs);
@@ -25,8 +17,11 @@ app.get("/", (req, res) => res.send("<h1> API de productos </h1>"));
 app.get("/productos", (req, res) => res.json(productos));
 app.post("/productos",(req, res) => {
     console.log("body: ", req.body);
-    
-    res.json({codigo: 3, nombre: "producto 3", precio: 30, cantidad:100});
+    lastId++;
+    const producto = {...req.body, codigo: lastId };
+    productos.push(producto);
+    res.status(201);
+    res.json({producto});
 });
 
 
